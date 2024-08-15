8d2e1ada7d1fe0dbf3c681122bd3a0907da88391
João Costa <jdpc557@gmail.com>
2024-08-15 19:17:01 +0100

    Add basic settings dialog

--- src/main/scala/eu/joaocosta/coffee/CoffeeTracker.scala
+++ src/main/scala/eu/joaocosta/coffee/CoffeeTracker.scala
@@ -30,16 +30,20 @@ object CoffeeTracker extends TyrianIOApp[Msg, Model]:
     case Msg.RemoveCheckIn(checkIn) =>
       (model.copy(history = model.history.removeCheckIn(checkIn)), Cmd.None)
     case Msg.OpenCheckInModal =>
-      (model.copy(checkInModal = true), Cmd.None)
+      (model.copy(checkInModal = true, settingsModal = false), Cmd.None)
     case Msg.CloseCheckInModal =>
       (model.copy(checkInModal = false), Cmd.None)
+    case Msg.OpenSettingsModal =>
+      (model.copy(settingsModal = true, checkInModal = false), Cmd.None)
+    case Msg.CloseSettingsModal =>
+      (model.copy(settingsModal = false), Cmd.None)
     case Msg.NoOp => (model, Cmd.None)
 
   def renderDrink(drink: Drink): Html[Msg] =
     Material.dropdown()(
       Material.listItem(
         attribute("slot", "trigger"),
-        attribute("icon", "add"),
+        attribute("icon", "add")
       )(span()(drink.name)),
       Material.menu()(
         drink.commonSizes.map((size, volume) =>
@@ -75,33 +79,42 @@ object CoffeeTracker extends TyrianIOApp[Msg, Model]:
   def renderHistory(history: History): Html[Msg] =
     div(style(Style("padding-bottom" -> "4em")))(
       h2("History") ::
-      history.checkIns
-        .groupBy(_.dateTime.toLocalDate())
-        .toList
-        .sortBy(_._1)
-        .map((localDate, checkIns) =>
-          Material.card()(
-            div(style(Style("padding" -> "1rem")))(
-              h3(localDate.toString),
-              Material.list()(checkIns.map(renderCheckIn))
+        history.checkIns
+          .groupBy(_.dateTime.toLocalDate())
+          .toList
+          .sortBy(_._1)
+          .map((localDate, checkIns) =>
+            Material.card()(
+              div(style(Style("padding" -> "1rem")))(
+                h3(localDate.toString),
+                Material.list()(checkIns.map(renderCheckIn))
+              )
             )
           )
-        )
     )
 
   def renderStats(history: History, settings: Settings): Html[Msg] =
     val plot = CaffeinePlot.getImage(history, settings, 1024, 512)
     Material.card()(
-      img(src := plot.src, style(Style("max-width"-> "100%"))),
+      img(src := plot.src, style(Style("max-width" -> "100%"))),
       div(style(Style("padding" -> "1rem")))(
         h2("Overview"),
-        span(f"Current caffeine: ${history.caffeineAt(Instant.now(), settings)}%1.1f mg")
+        span(
+          f"Current caffeine: ${history.caffeineAt(Instant.now(), settings)}%1.1f mg"
+        )
       )
     )
 
   def view(model: Model): Html[Msg] =
     Material.layout()(
-      Material.topAppBar()(h1()("Coffee Tracker")),
+      Material.topAppBar()(
+        Material.topAppBarTitle()("Coffee Tracker"),
+        div(style(Style("flex-grow" -> "1")))(),
+        Material.buttonIcon(
+          attribute("icon", "settings"),
+          onClick(Msg.OpenSettingsModal)
+        )()
+      ),
       Material.layoutMain()(
         renderStats(model.history, model.settings),
         renderHistory(model.history),
@@ -111,6 +124,13 @@ object CoffeeTracker extends TyrianIOApp[Msg, Model]:
         )(
           h2("Add Drink:"),
           renderDrinks
+        ),
+        Material.dialog(
+          attribute("open", model.settingsModal.toString),
+          onEvent("overlay-click", _ => Msg.CloseSettingsModal)
+        )(
+          h2("Settings"),
+          span()(model.settings.toString)
         )
       ),
       Material.fab(
@@ -132,7 +152,8 @@ object CoffeeTracker extends TyrianIOApp[Msg, Model]:
 final case class Model(
     history: History = History(),
     settings: Settings = Settings(),
-    checkInModal: Boolean = false
+    checkInModal: Boolean = false,
+    settingsModal: Boolean = false
 )
 
 enum Msg:
@@ -140,4 +161,6 @@ enum Msg:
   case RemoveCheckIn(checkIn: CheckIn)
   case OpenCheckInModal
   case CloseCheckInModal
+  case OpenSettingsModal
+  case CloseSettingsModal
   case NoOp
--- src/main/scala/eu/joaocosta/coffee/Material.scala
+++ src/main/scala/eu/joaocosta/coffee/Material.scala
@@ -7,20 +7,22 @@ object Material:
   def button[M](attributes: Attr[M]*)(text: String) =
     raw[M]("mdui-button", attributes.toList)(text)
   val buttonIcon = TagBuilder("mdui-button-icon")
-  val fab = TagBuilder("mdui-fab")
+  val fab        = TagBuilder("mdui-fab")
 
   val dropdown = TagBuilder("mdui-dropdown")
-  val menu = TagBuilder("mdui-menu")
+  val menu     = TagBuilder("mdui-menu")
   def menuItem[M](attributes: Attr[M]*)(text: String) =
     raw[M]("mdui-menu-item", attributes.toList)(text)
 
-  val list = TagBuilder("mdui-list")
+  val list     = TagBuilder("mdui-list")
   val listItem = TagBuilder("mdui-list-item")
 
   val card = TagBuilder("mdui-card")
 
-  val layout = TagBuilder("mdui-layout")
-  val topAppBar = TagBuilder("mdui-top-app-bar")
+  val layout     = TagBuilder("mdui-layout")
   val layoutMain = TagBuilder("mdui-layout-main")
+  val topAppBar  = TagBuilder("mdui-top-app-bar")
+  def topAppBarTitle[M](attributes: Attr[M]*)(text: String) =
+    raw[M]("mdui-top-app-bar-title", attributes.toList)(text)
 
   val dialog = TagBuilder("mdui-dialog")
